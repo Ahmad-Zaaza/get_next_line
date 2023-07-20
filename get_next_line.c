6946e874a952c895e17ac7ad6ec6e46d0a019303
Ahmad <ahmad94.dev@gmail.com>
2023-07-21 01:03:21 +0400

    extract first line

--- get_next_line.c
+++ get_next_line.c
@@ -12,81 +12,103 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	static t_list	*store;
-	t_list			*newline_node;
-	char			*line;
-	int				bytes_read;
+char *get_next_line(int fd) {
+  static t_list *store;
+  t_list *newline_node;
+  int node_index;
+  char *line;
+  int bytes_read;
 
-	// check if fd is negative, if BUFFER_SIZE is negative, or we don't have
-	// permissions to read the file
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
-		return (NULL);
-	bytes_read = 1;
-	line = NULL;
-	// 1. Read from fd until the buffer size and store.
-	read_and_store(&store, &bytes_read, fd);
-	newline_node = find_newline_node(&store);
-	if (newline_node)
-	{
-    
-	}
-	ft_print_lst(&store);
-	return (line);
+  // check if fd is negative, if BUFFER_SIZE is negative, or we don't have
+  // permissions to read the file
+  if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
+    return (NULL);
+  bytes_read = 1;
+  node_index = 0;
+  line = NULL;
+  // 1. Read from fd until the buffer size and store.
+  read_and_store(&store, &bytes_read, fd);
+  newline_node = find_newline_node(&store, &node_index);
+  if (newline_node) {
+    line = get_line(newline_node, node_index);
+  }
+  //   ft_print_lst(&store);
+  return (line);
 }
 
-void	read_and_store(t_list **store, int *p_read, int fd)
-{
-	char	*buffer;
+char *get_line(t_list *newline_node, int node_index) {
+  t_list *current;
+  char *line;
+  int i;
 
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buffer)
-		return ;
-	while ((*p_read = read(fd, buffer, BUFFER_SIZE)))
-	{
-		printf("read: %d\n", *p_read);
-		buffer[*p_read] = '\0';
-		add_to_store(store, buffer);
-	}
+  i = 0;
+  line = (char *)malloc(sizeof(char) * (BUFFER_SIZE * node_index + 1) + 1);
+  if (!line)
+    return (NULL);
+  current = ft_lstfirst(newline_node);
+  while (current) {
+    if (current == newline_node) {
+      while (newline_node->content[i]) {
+        if (newline_node->content[i] == '\n') {
+          ft_strncat(line, newline_node->content, i);
+          lstclear_prev(newline_node);
+          // memory leeks. should clear up the previous content
+          newline_node->content = ft_strdup(newline_node->content + i + 1);
+          return (line);
+        } else
+          i++;
+      }
+    } else
+      ft_strlcat(line, current->content, (BUFFER_SIZE * node_index + 1) + 1);
+    current = current->next;
+  }
+  return line;
 }
 
-t_list	*find_newline_node(t_list **store)
-{
-	t_list	*tmp;
-	int		i;
+void read_and_store(t_list **store, int *p_read, int fd) {
+  char *buffer;
 
-	i = 0;
-	tmp = *store;
-	while (tmp)
-	{
-		i = 0;
-		while (tmp->content[i])
-		{
-			if (tmp->content[i] == '\n')
-				return (tmp);
-			i++;
-		}
-		tmp = tmp->next;
-	}
-	return (NULL);
+  buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+  if (!buffer)
+    return;
+  while ((*p_read = read(fd, buffer, BUFFER_SIZE))) {
+    printf("read: %d\n", *p_read);
+    buffer[*p_read] = '\0';
+    add_to_store(store, buffer);
+  }
 }
 
-int	main(void)
-{
-	int fd;
-	char *line;
-	fd = open("./test.txt", O_RDONLY);
+t_list *find_newline_node(t_list **store, int *node_index) {
+  t_list *tmp;
+  int i;
 
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-		{
-			break ;
-		}
-		// printf("%s\n", line);
-	}
-	free(line);
-	return (0);
+  i = 0;
+  tmp = *store;
+  while (tmp) {
+    i = 0;
+    while (tmp->content[i]) {
+      if (tmp->content[i] == '\n')
+        return (tmp);
+      i++;
+    }
+    *node_index += 1;
+    tmp = tmp->next;
+  }
+  return (NULL);
+}
+
+int main(void) {
+  int fd;
+  char *line;
+  fd = open("./test.txt", O_RDONLY);
+
+  while (1) {
+    line = get_next_line(fd);
+    if (!line) {
+      break;
+    }
+    printf("%s\n", line);
+  }
+  free(line);
+  return (0);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -12,6 +12,31 @@ t_list *ft_lstlast(t_list *lst) {
   }
   return (current);
 }
+t_list *ft_lstfirst(t_list *lst) {
+
+  t_list *current;
+
+  if (!lst)
+    return (NULL);
+  current = lst;
+  while (current->prev) {
+    current = current->prev;
+  }
+  return (current);
+}
+
+void lstclear_prev(t_list *node) {
+  t_list *current;
+  t_list *tmp;
+
+  current = node->prev;
+  while (current) {
+    tmp = current->prev;
+    free(current->content);
+    free(current);
+    current = tmp;
+  }
+}
 
 t_list *ft_lstnew(char *content) {
   t_list *lst;
@@ -21,6 +46,7 @@ t_list *ft_lstnew(char *content) {
     return (NULL);
   lst->content = content;
   lst->next = NULL;
+  lst->prev = NULL;
   return (lst);
 }
 
@@ -32,6 +58,7 @@ void ft_lstadd_back(t_list **store, t_list *lst) {
     *store = lst;
   } else {
     last = ft_lstlast(*store);
+    lst->prev = last;
     last->next = lst;
   }
 }
@@ -75,7 +102,7 @@ char *ft_strdup(char *content) {
   return str;
 }
 
-int ft_strlen(char *str) {
+int ft_strlen(const char *str) {
   int i;
 
   i = 0;
@@ -83,4 +110,59 @@ int ft_strlen(char *str) {
     i++;
   }
   return (i);
+}
+
+char *ft_strjoin(char const *s1, char const *s2) {
+  int s1_len;
+  int s2_len;
+  char *str;
+  int i;
+  int j;
+
+  if (!s1 || !s2)
+    return (NULL);
+  s1_len = ft_strlen(s1);
+  s2_len = ft_strlen(s2);
+  str = (char *)malloc(s1_len + s2_len + 1);
+  if (!str)
+    return (NULL);
+  i = 0;
+  j = 0;
+  while (s1[i])
+    str[j++] = s1[i++];
+  i = 0;
+  while (s2[i])
+    str[j++] = s2[i++];
+  str[j] = '\0';
+  return (str);
+}
+
+size_t ft_strlcat(char *dest, char *src, size_t dest_size) {
+  size_t i;
+  size_t dest_len;
+
+  if (dest_size == 0)
+    return (ft_strlen(src));
+  dest_len = ft_strlen(dest);
+  if (dest_size <= dest_len)
+    return (ft_strlen(src) + dest_size);
+  i = 0;
+  while (src[i] && dest_len < (dest_size - 1)) {
+    dest[dest_len++] = src[i++];
+  }
+  dest[dest_len] = '\0';
+  return (dest_len + ft_strlen(&src[i]));
+}
+
+size_t ft_strncat(char *dest, char *src, size_t n)
+{
+  size_t i;
+  size_t dest_len;
+
+  dest_len = ft_strlen(dest);
+  i = 0;
+  while (src[i] && i < n)
+    dest[dest_len++] = src[i++];
+  dest[dest_len] = '\0';
+  return (dest_len);
 }
