7332124f3665e1a4987afc08e3c92e5e88c80259
Ahmad Rajai Zaaza <azaaza@lab3r3s2.42abudhabi.ae>
2023-07-20 18:12:12 +0400

    add more stupid functions

--- get_next_line.c
+++ get_next_line.c
@@ -3,80 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahmadzaaza <ahmadzaaza@student.42.fr>      +#+  +:+       +#+        */
+/*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/18 23:46:35 by ahmadzaaza        #+#    #+#             */
-/*   Updated: 2023/07/19 00:19:46 by ahmadzaaza       ###   ########.fr       */
+/*   Updated: 2023/07/20 17:48:27 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd) {
-  static t_list *store;
-  char *line;
-  int bytes_read;
+char	*get_next_line(int fd)
+{
+	static t_list	*store;
+	t_list			*newline_node;
+	char			*line;
+	int				bytes_read;
 
-  // check if fd is negative, if BUFFER_SIZE is negative, or we don't have
-  // permissions to read the file
-  if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
-    return (NULL);
-  bytes_read = 1;
-  line = NULL;
-
-  // 1. Read from fd until the buffer size and store.
-  read_and_store(&store, &bytes_read, fd);
-
-  ft_print_lst(&store);
-  return line;
+	// check if fd is negative, if BUFFER_SIZE is negative, or we don't have
+	// permissions to read the file
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
+		return (NULL);
+	bytes_read = 1;
+	line = NULL;
+	// 1. Read from fd until the buffer size and store.
+	read_and_store(&store, &bytes_read, fd);
+	newline_node = find_newline_node(&store);
+	if (newline_node)
+	{
+    
+	}
+	ft_print_lst(&store);
+	return (line);
 }
 
-void read_and_store(t_list **store, int *p_read, int fd) {
-  char *buffer;
-  buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-  if (!buffer)
-    return;
+void	read_and_store(t_list **store, int *p_read, int fd)
+{
+	char	*buffer;
 
-  while ((*p_read = read(fd, buffer, BUFFER_SIZE - 1))) {
-    printf("read: %d\n", *p_read);
-    buffer[*p_read] = '\0';
-    add_to_store(store, buffer);
-  }
-  // add the buffer to the list
+	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return ;
+	while ((*p_read = read(fd, buffer, BUFFER_SIZE)))
+	{
+		printf("read: %d\n", *p_read);
+		buffer[*p_read] = '\0';
+		add_to_store(store, buffer);
+	}
 }
 
-int found_newline(t_list *store) {
-  t_list *tmp;
-  int i;
+t_list	*find_newline_node(t_list **store)
+{
+	t_list	*tmp;
+	int		i;
 
-  i = 0;
-  store = tmp;
-  while (tmp) {
-    i = 0;
-    while (tmp->content[i]) {
-      if (tmp->content[i] == '\n') {
-        return (1);
-      }
-      i++;
-    }
-    tmp = tmp->next;
-  }
-  return (0);
+	i = 0;
+	tmp = *store;
+	while (tmp)
+	{
+		i = 0;
+		while (tmp->content[i])
+		{
+			if (tmp->content[i] == '\n')
+				return (tmp);
+			i++;
+		}
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
 
-int main(void) {
+int	main(void)
+{
+	int fd;
+	char *line;
+	fd = open("./test.txt", O_RDONLY);
 
-  int fd;
-  char *line;
-  fd = open("./test.txt", O_RDONLY);
-
-  while (1) {
-    line = get_next_line(fd);
-    if (!line) {
-      break;
-    }
-    // printf("%s\n", line);
-  }
-  free(line);
-  return (0);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+		{
+			break ;
+		}
+		// printf("%s\n", line);
+	}
+	free(line);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -3,40 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahmadzaaza <ahmadzaaza@student.42.fr>      +#+  +:+       +#+        */
+/*   By: azaaza <azaaza@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/19 00:20:26 by ahmadzaaza        #+#    #+#             */
-/*   Updated: 2023/07/19 00:21:21 by ahmadzaaza       ###   ########.fr       */
+/*   Updated: 2023/07/20 17:48:35 by azaaza           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 5
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
-#include "fcntl.h"
-#include "stdlib.h"
-#include "unistd.h"
-#include "stdio.h"
+# include "fcntl.h"
+# include "stdio.h"
+# include "stdlib.h"
+# include "unistd.h"
 
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}					t_list;
 
-typedef struct s_list {
-  char *content;
-  struct s_list *next;
-} t_list;
+void				read_and_store(t_list **store, int *p_read, int fd);
+char				*get_next_line(int fd);
 
-void read_and_store(t_list **store, int *p_read, int fd);
-char *get_next_line(int fd);
+t_list				*ft_lst_last(t_list *lst);
+t_list				*ft_lstnew(char *content);
+void				ft_lstadd_back(t_list **store, t_list *lst);
+void				add_to_store(t_list **store, char *content);
 
-t_list *ft_lst_last(t_list *lst);
-t_list *ft_lstnew(char *content);
-void ft_lstadd_back(t_list **store, t_list *lst);
-void add_to_store(t_list **store, char *content);
+t_list				*find_newline_node(t_list **store);
+t_list				*find_newline_node(t_list **store);
+// int					found_newline(t_list *store);
 
-char *ft_strdup(char *content);
-int ft_strlen(char *str);
-void ft_print_lst(t_list **store);
+char				*ft_strdup(char *content);
+int					ft_strlen(char *str);
+void				ft_print_lst(t_list **store);
 #endif
