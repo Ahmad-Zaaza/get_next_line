ff2fb692374f96b075443f19ff39f45b86a6c9b9
Ahmad <ahmad94.dev@gmail.com>
2023-07-22 13:21:47 +0400

    get all lines

--- get_next_line.c
+++ get_next_line.c
@@ -12,108 +12,98 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
-{
-	static t_list	*store;
-	t_list			*newline_node;
-	int				node_index;
-	char			*line;
+char *get_next_line(int fd) {
+  static t_list *store;
+  t_list *newline_node;
+  int node_index;
+  char *line;
 
-	// check if fd is negative, if BUFFER_SIZE is negative, or we don't have
-	// permissions to read the file
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
-		return (NULL);
-	node_index = 0;
-	line = NULL;
-	// 1. Read from fd until the buffer size and store.
-	read_and_store(&store, fd);
-	line = get_line(&store);
-	//   ft_print_lst(&store);
-	return (line);
+  // check if fd is negative, if BUFFER_SIZE is negative, or we don't have
+  // permissions to read the file
+  if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, line, 0) < 0)
+    return (NULL);
+  node_index = 0;
+  line = NULL;
+  // 1. Read from fd until the buffer size and store.
+  read_and_store(&store, fd);
+  line = get_line(&store);
+  return (line);
 }
 
-char	*get_line(t_list **store, int count)
-{
-	t_list	*current;
-	char	*line;
-	int		node_count;
+char *get_line(t_list **store) {
+  t_list *current;
+  int count;
 
-	line = (char *)malloc(sizeof(char) * count + 1);
-	if (!line)
-		return (NULL);
-	current = *store;
-	while (node_count <= count)
-	{
-		line[i] = 
-		node_count++;
-		current = current->next;
-	}
-	return (line);
+  current = *store;
+  count = 1;
+
+  while (current) {
+    if (current->content[0] == '\n' || !current->next) {
+      return fill_and_purge(store, count);
+    }
+    current = current->next;
+    count++;
+  }
+  return (NULL);
+}
+char *fill_and_purge(t_list **store, int node_index) {
+  t_list *tmp;
+  int i;
+  char *line;
+
+  line = (char *)malloc(sizeof(char) * node_index);
+  if (!line)
+    return (NULL);
+  i = 0;
+  line[node_index] = '\0';
+  while (*store) {
+    if ((*store)->content[0] == '\n') {
+      tmp = (*store)->next;
+      free((*store)->content);
+      free((*store));
+      *store = tmp;
+      break;
+    } else {
+      line[i] = (*store)->content[0];
+      tmp = (*store)->next;
+      free((*store)->content);
+      free((*store));
+      *store = tmp;
+      i++;
+    }
+  }
+  return (line);
+}
+void read_and_store(t_list **store, int fd) {
+  char *buffer;
+  int readed;
+  int i;
+
+  buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+  if (!buffer)
+    return;
+  while ((readed = read(fd, buffer, BUFFER_SIZE))) {
+    i = 0;
+    buffer[readed] = '\0';
+    while (buffer[i]) {
+      ft_lstadd_back(store, ft_lstnew(ft_strdup(&buffer[i])));
+      i++;
+    }
+  }
 }
 
-void	read_and_store(t_list **store, int fd)
-{
-	char	*buffer;
-	int		readed;
-	int		i;
-	char	*line;
+int main(void) {
+  int fd;
+  char *line;
+  fd = open("./test.txt", O_RDONLY);
 
-	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (!buffer)
-		return ;
-	while ((readed = read(fd, buffer, BUFFER_SIZE)))
-	{
-		buffer[readed] = '\0';
-		while (buffer[i])
-		{
-			if (buffer[i] == '\n')
-			{
-				line = get_line(store, i)
-			}
-			else
-				ft_lstadd_back(store, ft_lstnew(&buffer[i]));
-			i++;
-		}
-	}
-}
-
-t_list	*find_newline_node(t_list **store, int *node_index)
-{
-	t_list	*tmp;
-	int		i;
-
-	i = 0;
-	tmp = *store;
-	while (tmp)
-	{
-		i = 0;
-		while (tmp->content[i])
-		{
-			if (tmp->content[i] == '\n')
-				return (tmp);
-			i++;
-		}
-		*node_index += 1;
-		tmp = tmp->next;
-	}
-	return (NULL);
-}
-
-int	main(void)
-{
-	int fd;
-	char *line;
-	fd = open("./test.txt", O_RDONLY);
-
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (!line)
-		{
-			break ;
-		}
-		printf("%s\n", line);
-	}
-	free(line);
-	return (0);
+  while (1) {
+    line = get_next_line(fd);
+    if (!line) {
+      break;
+    }
+    printf("%s\n", line);
+  }
+  free(line);
+  return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -11,41 +11,41 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
-# endif
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 5
+#endif
 
-# include "fcntl.h"
-# include "stdio.h"
-# include "stdlib.h"
-# include "unistd.h"
+#include "fcntl.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "unistd.h"
 
-typedef struct s_list
-{
-	char			*content;
-	struct s_list	*next;
-	struct s_list	*prev;
-}					t_list;
+typedef struct s_list {
+  char *content;
+  struct s_list *next;
+  struct s_list *prev;
+} t_list;
 
-void				read_and_store(t_list **store, int fd);
-char				*get_next_line(int fd);
+void read_and_store(t_list **store, int fd);
+char *get_next_line(int fd);
 
-t_list				*ft_lst_last(t_list *lst);
-t_list				*ft_lstfirst(t_list *lst);
-t_list				*ft_lstnew(char *content);
-void				ft_lstadd_back(t_list **store, t_list *lst);
-void				add_to_store(t_list **store, char *content);
+t_list *ft_lst_last(t_list *lst);
+t_list *ft_lstfirst(t_list *lst);
+t_list *ft_lstnew(char *content);
+void ft_lstadd_back(t_list **store, t_list *lst);
+void add_to_store(t_list **store, char *content);
 
-char				*get_line(t_list **store, int count);
-void				lstclear_prev(t_list *node);
+char *get_line(t_list **store);
+void lstclear_prev(t_list *node);
+char *fill_and_purge(t_list **store, int node_index);
 // int					found_newline(t_list *store);
 
-size_t				ft_strncat(char *dest, char *src, size_t n);
-char				*ft_strdup(char *content);
-int					ft_strlen(const char *str);
-void				ft_print_lst(t_list **store);
-char				*ft_strjoin(char const *s1, char const *s2);
-size_t				ft_strlcat(char *dest, char *src, size_t dest_size);
+size_t ft_strncat(char *dest, char *src, size_t n);
+char *ft_strdup(char *content);
+int ft_strlen(const char *str);
+void ft_print_lst(t_list **store);
+char *ft_strjoin(char const *s1, char const *s2);
+size_t ft_strlcat(char *dest, char *src, size_t dest_size);
 #endif
